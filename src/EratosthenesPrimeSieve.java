892f979a58195fb65c18e51ec4049807890861a7
Franz Einböck <Einboeck.Franz@sus.htl-grieskirchen.at>
2020-09-23 11:51:26 +0200

    Language Level Changed to JDK 1.8, Implementation of those interface methods

--- src/EratosthenesPrimeSieve.java
+++ src/EratosthenesPrimeSieve.java
@@ -1,3 +1,19 @@
-public class EratosthenesPrimeSieve {
-    
+public class EratosthenesPrimeSieve implements PrimeSieve {
+    private final int bound;
+    private boolean[] primeNumbers;
+
+    public EratosthenesPrimeSieve(int bound) {
+        this.bound = bound;
+        primeNumbers = new boolean[bound+1];
+    }
+
+    @Override
+    public boolean isPrime(int p) {
+        return primeNumbers[p];
+    }
+
+    @Override
+    public void printPrimes() {
+        for (int i = 0; i < primeNumbers.length; i++) if(primeNumbers[i]) System.out.println(i);
+    }
 }
