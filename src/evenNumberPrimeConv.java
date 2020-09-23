a8d768bc9cf954fd21341051d7e30d2abdcd6954
Franz Einböck <Einboeck.Franz@sus.htl-grieskirchen.at>
2020-09-23 12:25:48 +0200

    Created Class evenNumberPrimeConv.java (2. Aufgabe) Created 3 Methods inside this class (main, run, getThosePrimeNumbers: int bound)

---
+++ src/evenNumberPrimeConv.java
@@ -0,0 +1,14 @@
+public class evenNumberPrimeConv {
+    public static void main(String[] args) {
+        //TODO Call run()
+    }
+
+    private void run() {
+        //TODO Application Logic
+    }
+
+    public int[] getThosePrimeNumbers(int bound) {
+        //TODO
+        return null;
+    }
+}
