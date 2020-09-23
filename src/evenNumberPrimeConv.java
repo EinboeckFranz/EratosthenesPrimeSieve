1ce6576ed636d14bbe424d4b77de25e8ca78ae7e
Franz Einböck <Einboeck.Franz@sus.htl-grieskirchen.at>
2020-09-23 12:30:36 +0200

    Imported Scanner, also declared a Scanner var and implemented methods main and run

--- src/evenNumberPrimeConv.java
+++ src/evenNumberPrimeConv.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class evenNumberPrimeConv {
+    private Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        //TODO Call run()
+        evenNumberPrimeConv main = new evenNumberPrimeConv();
+        main.run();
     }
 
     private void run() {
-        //TODO Application Logic
+        System.out.println("Enter the bound:");
+        getThosePrimeNumbers(Integer.parseInt(sc.nextLine()));
     }
 
     public int[] getThosePrimeNumbers(int bound) {
