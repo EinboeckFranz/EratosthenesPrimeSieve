8daadf38886d6f50b2dcd4496b085426bc231822
BuildTools <unconfigured@null.spigotmc.org>
2020-10-15 15:34:56 +0200

    **Project Finished** All other Classes (EratosthenesPrimeSieve.java & PrimeSieve.java) were moved into evenNumberPrimeConv.java

--- src/PrimeSieve.java
+++
@@ -1,4 +0,0 @@
-public interface PrimeSieve {
-    boolean isPrime(int p);
-    void printPrimes();
-}
--- src/evenNumberPrimeConv.java
+++ src/evenNumberPrimeConv.java
@@ -32,4 +32,40 @@ public class evenNumberPrimeConv {
             alreadyPrintedASum = false;
         }
     }
+
+    static class EratosthenesPrimeSieve implements PrimeSieve {
+        private final int bound;
+        private boolean[] primeNumbers;
+
+        public EratosthenesPrimeSieve(int bound) {
+            this.bound = bound;
+            primeNumbers = new boolean[bound+1];
+            getPrimeNumbers();
+        }
+
+        public void getPrimeNumbers() {
+            for (int i = 2; i < primeNumbers.length; i++)
+                primeNumbers[i] = true;
+
+            for (int i = 0; i < primeNumbers.length; i++) {
+                if(primeNumbers[i])
+                    for (int j = 2*i; j < primeNumbers.length; j += i)
+                        primeNumbers[j] = false;
+            }
+        }
+
+        @Override
+        public boolean isPrime(int p) {
+            return primeNumbers[p];
+        }
+
+        @Override
+        public void printPrimes() {
+            for (int i = 0; i < primeNumbers.length; i++) if(primeNumbers[i]) System.out.println(i);
+        }
+    }
+}
+interface PrimeSieve {
+    boolean isPrime(int p);
+    void printPrimes();
 }
